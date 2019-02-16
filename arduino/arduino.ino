9872595f816d291de8edc68b516b204aa461bcec
etnom <montgomery.okdaily@gmail.com>
2019-02-16 08:29:55 -0800

    pinmodes for motor pins

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,13 @@
-void setup() {
+#define PUSHER_MOTOR_PIN 3
+#define PUSHER_BRAKE_PIN 4
+#define FLYWHEEL_MOTOR_PIN 5
 
+void setup() {
+	pinMode(PUSHER_MOTOR_PIN, OUTPUT);
+	pinMode(PUSHER_BRAKE_PIN, OUTPUT);
+	pinMode(FLYWHEEL_MOTOR_PIN, OUTPUT);
 }
 
 void loop() {
-	
+
 }
