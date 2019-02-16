ffec723189605aaa9afb321bf628970ceeae426f
etnom <montgomery.okdaily@gmail.com>
2019-02-16 10:19:59 -0800

    implement first serial reading as command type

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,13 @@
 #include <Button.h>
 #include <Wire.h>
 
+// COmmand codes
+#define ROTATE_LEFT 1
+#define ROTATE_RIGHT 2
+#define ROTATE_UP 3
+#define ROTATE_DOWN 4
+#define SHOOT 5
+
 #define SLAVE_ADDRESS 0x04
 
 #define PUSHER_MOTOR_PIN 3
@@ -14,9 +21,13 @@
 #define PULLUP true
 #define DEBOUNCE_MS 20
 
+
+
 Button triggerBtn (PUSHER_RETURN_SWICH_PIN, PULLUP, INVERT, DEBOUNCE_MS);
 
 uint8_t pusherPow, flywheelPow;
+
+uint8_t commandType;	// buffer to indicate commandson which motor controls to complete
 
 void setup() {
 	Serial.begin(9600);
@@ -35,11 +46,28 @@ void loop() {
 
 void readSerial() {
 	if (Serial.available() > 0) {
-    int incomingByte = Serial.read();
+    char reading = Serial.read();
 
-    // say what you got:
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
+    // set type of command
+    if (commandType == 0) {
+    	commandType = reading -'0';
+  		Serial.print("Command: " + (String)commandType);
+    } else {
+    	if (command == ROTATE_LEFT) {
+    		
+    	} else if (command == ROTATE_RIGHT) {
+
+    	} else if (command == ROTATE_UP) {
+
+    	} else if (command == ROTATE_DOWN) {
+
+    	} else if (command == SHOOT) {
+
+    	}
+
+
+    	commandType = 0;
+    }
   }
 }
 
