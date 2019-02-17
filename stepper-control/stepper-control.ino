3d088844abd539b2375267116601ea2e2cda48c1
etnom <montgomery.okdaily@gmail.com>
2019-02-16 21:44:32 -0800

    stepper control

--- stepper-control/stepper-control.ino
+++ stepper-control/stepper-control.ino
@@ -1,7 +1,32 @@
-void setup() {
+#include <Stepper.h>
 
+#define STEPS 200
+
+
+Stepper stepper(STEPS, 4, 5, 6, 7);
+
+
+void setup() {
+	Serial.begin(9600);
+  // Serial.println("Stepper test!");
+  // set the speed of the motor to 30 RPMs
+  stepper.setSpeed(60);
 }
 
 void loop() {
-	
+	stepperRight(500);
+	// Serial.println("fads");
+	delay(500);
+	stepperLeft(500);
+	delay(500);
+}
+
+void stepperRight(int steps) {
+	Serial.println("stepper right");
+	stepper.step(steps);
+}	
+
+void stepperLeft(int steps) {
+	Serial.println("stepper left");
+	stepper.step(-steps);
 }
