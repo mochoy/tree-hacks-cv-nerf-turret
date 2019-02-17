21feb8837fdad7d11b3856f3746d1a65a1307817
etnom <montgomery.okdaily@gmail.com>
2019-02-17 00:42:55 -0800

    fix timing for firing, create serial-rx-test sketch

--- firing-control/firing-control.ino
+++ firing-control/firing-control.ino
@@ -1,9 +1,9 @@
 #define PUSHER_PIN 9
 #define FLYWHEEL_PIN 10
 
-#define FLYWHEEL_ACCEL_DELAY 50
-#define FLYWHEEL_FIRE_DELAY 250
-#define PUSHER_FIRE_DELAY 200
+#define FLYWHEEL_ACCEL_DELAY 150
+#define FIRE_DELAY 35
+
 
 void setup() {
 	pinMode(PUSHER_PIN, OUTPUT);
@@ -11,12 +11,19 @@ void setup() {
 }
 
 void loop() {
+	for (int i = 0; i < 10; i++) {
+		fire();
+	}
 
+	delay(5000);
 }
 
 void fire() {
 	digitalWrite(FLYWHEEL_PIN, HIGH);
 	delay(FLYWHEEL_ACCEL_DELAY);
-	digitalWrite(PUSHER_PIN);
-	delay(FLYWHEEL_FIRE_DELAY);
+	digitalWrite(PUSHER_PIN, HIGH);
+	delay(FIRE_DELAY);
+
+	digitalWrite(FLYWHEEL_PIN, LOW);
+	digitalWrite(PUSHER_PIN, LOW);
 }
