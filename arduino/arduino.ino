c2fbc4dd5bcf0eaa158a28a5c7f626ee42e23b93
etnom <montgomery.okdaily@gmail.com>
2019-02-17 02:18:31 -0800

    working controls with buffer clearing

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,21 +1,23 @@
 #include <Button.h>
 #include <Stepper.h>
 
-// 1Ommand codes
-#define ROTATE_LEFT_COMMAND 1
-#define ROTATE_RIGHT_COMMAND 2
-#define SHOOT_COMMAND 3
-
 
 #define PUSHER_MOTOR_PIN 9
 #define FLYWHEEL_MOTOR_PIN 10
 
+#define FLYWHEEL_ACCEL_DELAY 150
+#define FIRE_DELAY 35
+
 #define STEPS 200
-#define STEPS_ON_ROTATION 
+#define STEP_SPEED 4
 
 
 Stepper stepper(STEPS, 4, 5, 6, 7);
+
 uint8_t pusherPow, flywheelPow;
+String command = "";
+
+bool toClearSerialBuffer = false;
 
 void setup() {
 	Serial.begin(9600);
@@ -23,42 +25,79 @@ void setup() {
 	pinMode(PUSHER_MOTOR_PIN, OUTPUT);
 	pinMode(FLYWHEEL_MOTOR_PIN, OUTPUT);
 
-	stepper.setSpeed(4);
+	stepper.setSpeed(STEP_SPEED);
 
-	// Wire.begin(SLAVE_ADDRESS);
-	// Wire.onReceive(receiveI2CData);
 }
 
 void loop() {
+	if (toClearSerialBuffer) {
+		if (Serial.available() > 0) {
+			Serial.println("clearing");
+
+			while (Serial.available() > 0) {
+				Serial.read();
+			}	
+		}
+
+		toClearSerialBuffer = false;
+	} 
+
 	readSerial();
 }
 
 void readSerial() {
 	if (Serial.available() > 0) {
     char reading = Serial.read();
-    int intReading = reading -'0';
+    int intReading = reading - '0';
 
-    if (reading == ROTATE_LEFT_COMMAND) {
-    	rotateLeft();
-    } else if (reading == ROTATE_RIGHT_COMMAND) {
-    	rotateRight();
-    } else if (reading == SHOOT_COMMAND) {
-    	shoot();
-    }
+  	Serial.print("Reading: ");
+		Serial.println(reading);
+		Serial.print("Command: ");
 
+    // null terminator
+		if (reading == 'a') {
+			Serial.println(command);
+
+			Serial.println("executing command");
+
+			int degreesToTurn = command.toInt();
+			Serial.print("rotation: ");
+			Serial.println((String)(degreesToTurn + 1));
+
+			command = "";
+			Serial.println("resetting");
+
+			rotate(degreesToTurn);
+
+			return;
+		} 
+
+		//appending to command
+		command += reading;
+		Serial.println(command);
+ 
   }
 }
 
-void rotateLeft() {
-
-}
-
-void rotateRight() {
-
+void rotate(int stepsToRotate) {
+	stepper.step(stepsToRotate);
+	shoot();
 }
 
 void shoot() {
+	Serial.print("shooting");
 
+	digitalWrite(PUSHER_MOTOR_PIN, HIGH);
+	delay(FLYWHEEL_ACCEL_DELAY);
+	digitalWrite(PUSHER_MOTOR_PIN, HIGH);
+	delay(FIRE_DELAY);
+
+	digitalWrite(PUSHER_MOTOR_PIN, LOW);
+	digitalWrite(PUSHER_MOTOR_PIN, LOW);
+
+	Serial.println("finished\n\n");
+
+	toClearSerialBuffer = true;
 }
 
 // void receiveI2CData () {
@@ -66,6 +105,6 @@ void shoot() {
 //     int reading = Wire.read();
 //     Serial.print("data received: ");
 //     Serial.println(reading);
-8
+// 8
 //   }
 // }
