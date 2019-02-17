8fd5ea9f0019e0b02a41ef95f2140db226efd7cf
etnom <montgomery.okdaily@gmail.com>
2019-02-16 23:13:35 -0800

    fire method in fire control

--- firing-control/firing-control.ino
+++ firing-control/firing-control.ino
@@ -1,10 +1,22 @@
 #define PUSHER_PIN 9
 #define FLYWHEEL_PIN 10
 
-void setup() {
+#define FLYWHEEL_ACCEL_DELAY 50
+#define FLYWHEEL_FIRE_DELAY 250
+#define PUSHER_FIRE_DELAY 200
 
+void setup() {
+	pinMode(PUSHER_PIN, OUTPUT);
+	pinMode(FLYWHEEL_PIN, OUTPUT);
 }
 
 void loop() {
 
+}
+
+void fire() {
+	digitalWrite(FLYWHEEL_PIN, HIGH);
+	delay(FLYWHEEL_ACCEL_DELAY);
+	digitalWrite(PUSHER_PIN);
+	delay(FLYWHEEL_FIRE_DELAY);
 }
